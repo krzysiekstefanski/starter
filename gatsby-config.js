a9e9353c52f9d474a30fed3de0c2f6d98c607e6a
Riotters <krzysztof@riotters.com>
2023-07-13 13:00:24 +0200

    starter

---
+++ gatsby-config.js
@@ -0,0 +1,52 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-intl`,
+      options: {
+        path: `${__dirname}/src/intl`,
+        languages: [`pl`, `en`],
+        defaultLanguage: `pl`,
+      },
+    },
+    // {
+    //   resolve: `gatsby-plugin-offline`,
+    // },
+    `gatsby-plugin-styled-components`,
+  ],
+}
---
+++ src/pages/using-ssr.js
@@ -0,0 +1,97 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+import styled from "styled-components"
+
+const ProductsList = styled.div`
+  display: grid;
+  grid-template-columns: 25% 25% 25% 25%;
+  grid-gap: 15px;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  gap: 10px;
+  padding: 10px;
+  border: 1px solid #eee;
+
+  &:hover {
+    border-color: #ddd;
+  }
+`
+
+const ImageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+  width: 100%;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    margin: 0;
+  }
+`
+
+const Price = styled.span`
+  font-size: 14px;
+  font-weight: 600;
+  color: #030;
+  margin-top: auto;
+  align-self: flex-end;
+`;
+
+const UsingSSR = ({ serverData }) => {
+  console.log(serverData);
+
+  return (
+    <Layout>
+      <h1>
+        Produkty
+      </h1>
+      <ProductsList>
+        {serverData.map(product => (
+          <Wrapper>
+            <ImageWrapper>
+              <img src={product.image} />
+            </ImageWrapper>
+            <span>{product.title}</span>
+            <Price>${product.price}</Price>
+          </Wrapper>
+        ))}
+      </ProductsList>
+    </Layout>
+  )
+}
+
+export const Head = () => <Seo title="Using SSR" />
+
+export default UsingSSR
+
+export async function getServerData() {
+  try {
+    const res = await fetch(`https://fakestoreapi.com/products`)
+    if (!res.ok) {
+      throw new Error(`Response failed`)
+    }
+    return {
+      props: await res.json(),
+    }
+  } catch (error) {
+    return {
+      status: 500,
+      headers: {},
+      props: {},
+    }
+  }
+}
